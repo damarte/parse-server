8404dd4fa59f7e1c154714b819b475811f4e8d80
Seiji Akiyama <seiji.akiyama2.0@gmail.com>
2017-12-15 13:28:01 -0200

    Update ImportRouter.js

--- src/Routers/ImportRouter.js
+++ src/Routers/ImportRouter.js
@@ -49,7 +49,7 @@ function handleImport(req, res) {
       })
       .catch((error) => {
         emailControllerAdapter.sendMail({
-          text: `We could not import your data to the class ${req.params.className}. Error: ${error}`,
+          text: `We could not import your data to the class ${req.params.className}. Error: ${JSON.stringify(error)}`,
           to: req.body.feedbackEmail,
           subject: 'Import failed'
         });
